5bd250786954151b2ebb8b2d676a70834885fab6
Prabin Dangol <neprabin@gmail.com>
2023-02-23 17:44:45 +0545

    v 2.0.21_

--- inc/customizer/customizer-panel/settings.php
+++ inc/customizer/customizer-panel/settings.php
@@ -1016,13 +1016,13 @@ $wp_customize->add_control('square_social_instagram', array(
     'label' => esc_html__('Instagram', 'square')
 ));
 
-$wp_customize->add_setting('squarepress_social_upgrade_text', array(
+$wp_customize->add_setting('square_social_upgrade_text', array(
     'sanitize_callback' => 'square_sanitize_text'
 ));
 
-$wp_customize->add_control(new Square_Upgrade_Info_Control($wp_customize, 'squarepress_social_upgrade_text', array(
+$wp_customize->add_control(new Square_Upgrade_Info_Control($wp_customize, 'square_social_upgrade_text', array(
     'section' => 'square_social_sec',
-    'label' => esc_html__('To add unlimited and any social media,', 'squarepress'),
+    'label' => esc_html__('To add unlimited and any social media,', 'square'),
     'priority' => 100,
     'active_callback' => 'square_is_upgrade_notice_active',
     'upgrade_text' => esc_html__('Upgrade to PRO', 'square'),

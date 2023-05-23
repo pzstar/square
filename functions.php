401b097c58c6e2c784ab36d185a2f1cea367f875
prabin <prabin@macbook-pro.domain.name>
2023-05-23 23:17:11 +0545

    v 2.0.5

--- functions.php
+++ functions.php
@@ -326,6 +326,21 @@ if (!function_exists('wp_body_open')) {
 
 }
 
+add_filter('template_include', 'square_frontpage_template', 9999);
+
+function square_frontpage_template($template) {
+    if (is_front_page()) {
+        $enable_frontpage = get_theme_mod('square_enable_frontpage', false);
+        if ($enable_frontpage) {
+            $new_template = locate_template(array('templates/home-template.php'));
+            if ('' != $new_template) {
+                return $new_template;
+            }
+        }
+    }
+    return $template;
+}
+
 /**
  * Custom template tags for this theme.
  */

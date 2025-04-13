d3289d467fa0d527baead05ec0fd428046c61fec
Prabin Dangol <neprabin@gmail.com>
2025-04-13 17:35:28 +0545

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -58,7 +58,7 @@ if (!function_exists('square_entry_footer')) {
         if ('post' === get_post_type()) {
             /* translators: used between list items, there is a space after the comma */
             $categories_list = get_the_category_list(', ');
-            if ($categories_list && square_categorized_blog()) {
+            if ($categories_list) {
                 echo '<span class="cat-links"><i class="fa-solid fa-folder"></i>' . $categories_list . '</span>'; // WPCS: XSS OK.
             }
 
@@ -71,50 +71,6 @@ if (!function_exists('square_entry_footer')) {
     }
 
 }
-
-/**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
-function square_categorized_blog() {
-    if (false === ($all_the_cool_cats = get_transient('square_categories'))) {
-        // Create an array of all the categories that are attached to posts.
-        $all_the_cool_cats = get_categories(array(
-            'fields' => 'ids',
-            'hide_empty' => 1,
-            // We only need to know if there is more than one category.
-            'number' => 2,
-        ));
-
-        // Count the number of categories that are attached to the posts.
-        $all_the_cool_cats = count($all_the_cool_cats);
-
-        set_transient('square_categories', $all_the_cool_cats);
-    }
-
-    if ($all_the_cool_cats > 1) {
-        // This blog has more than 1 category so square_categorized_blog should return true.
-        return true;
-    } else {
-        // This blog has only 1 category so square_categorized_blog should return false.
-        return false;
-    }
-}
-
-/**
- * Flush out the transients used in square_categorized_blog.
- */
-function square_category_transient_flusher() {
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return;
-    }
-    // Like, beat it. Dig?
-    delete_transient('square_categories');
-}
-
-add_action('edit_category', 'square_category_transient_flusher');
-add_action('save_post', 'square_category_transient_flusher');
 
 if (!function_exists('square_social_share')) {
 
